292f359c3f3271c6c72dba8aaf6944d560c1017c
SimplyyJoji <59666562+SimplyyJoji@users.noreply.github.com>
2021-10-19 18:29:35 -0700

    added Node Class and the BinarySearchTree class

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+
+/*
+Root- The top node in a tree
+Child- A node directly connected to another node
+Parent- A mode that has node below it
+Leaf- A node with no children
+Edge-The connecton line between
+*/
+
+class Node {
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+  class BinarySearchTree {
+    constructor(){
+      this.root = null;
+
+    }
+  }
+
+
+function insert(item) {
+  let newNode = new Node(value);
+  
+  if (this.root.length < 1 ) {
+    this.root = newNode;
+  }
+
+}
+
 function bfs () {
   var node = this.root;
   var queue = [];
